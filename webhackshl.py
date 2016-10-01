b0bfe5d5f9974637e06f94f031e266604f37540a
Security Hack Labs <securityhacklabs@cock.lu>
2016-10-01 16:35:27 -0500

    Update webhackshl.py

--- webhackshl.py
+++ webhackshl.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python2
 # encoding: utf-8
-# Testing Web Framework - Copyright (C) <2016> - <Hanom1960>
+# Testing Web Framework - Copyright (C) <2016>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
